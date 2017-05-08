3b79d72edb1bb0b242d5421d6006b99182ac1fd3
Dheera Venkatraman <dheera@dheera.net>
2017-05-08 04:50:19 +0000

    colors

--- README.md
+++ README.md
@@ -12,13 +12,13 @@ from TermGraphics import TermGraphics
 g = TermGraphics()
 
 g.clear()
+
 for x in range(g.shape[0]):
     y = int(0.5 * g.shape[1] * ((2*x/g.shape[0]-1)**3 + 1))
     g.point((x,y))
 
 g.draw()
 time.sleep(5)
-
 ```
 
 ![screenshot](/screenshot0.png?raw=true "screenshot")
@@ -32,7 +32,40 @@ import time
 import TermGraphics
 
 g = TermGraphics.TermGraphics(mode = TermGraphics.MODE_EASCII)
+
+g.clear()
+
+for x in range(g.shape[0]):
+    y = int(0.5 * g.shape[1] * ((2*x/g.shape[0]-1)**3 + 1))
+    g.point((x,y))
+
+g.draw()
+time.sleep(5)
 ```
 
 ![screenshot](/screenshot1.png?raw=true "screenshot")
 
+It also supports changing colors as per ANSI terminal specifications. However, since it is only possible to set the color of a full character, if 2 points of different colors occur in the same character block, the latter color will be used.
+
+```
+g = TermGraphics.TermGraphics()
+
+g.clear()
+
+g.set_color(TermGraphics.COLOR_RED)
+
+for x in range(g.shape[0]):
+    y = int(0.5 * g.shape[1] * ((2*x/g.shape[0]-1)**3 + 1))
+    g.point((x,y))
+
+g.set_color(TermGraphics.COLOR_BLUE)
+
+for x in range(g.shape[0]):
+    y = int(0.5 * g.shape[1] * (-(2*x/g.shape[0]-1)**3 + 1))
+    g.point((x,y))
+
+g.draw()
+time.sleep(5)
+```
+
+![screenshot](/screenshot2.png?raw=true "screenshot")
--- TermGraphics.py
+++ TermGraphics.py
@@ -5,10 +5,30 @@ import os
 import sys
 import time
 
+COLOR_BLACK = 0
+COLOR_RED = 1
+COLOR_GREEN = 2
+COLOR_YELLOW = 3
+COLOR_BLUE = 4
+COLOR_MAGENTA = 5
+COLOR_CYAN = 6
+COLOR_WHITE = 7
+
 MODE_BRAILLE = 0
 MODE_EASCII = 2
 
 TABLE_EASCII = " '-'.*.|'~/~/F//-\\-~/>-&'\"\"\"/)//.\\\\\\_LLL'\"<C-=CC:\\-\\vD=D|Y|Y|)AH.!i!.ii|/\"/F/Fff//rkfPrkJJ/P/P/P//>brr>kl>&&*=fF/)vb/PPDJ)19/2/R.\\\\\\\\\\\\(=T([(((C=3-5cSct!919|7Ce,\\\\\\_\\\\\\i919i9(C|)\\\\+tv\\|719|7@9_L=L_LLL_=6[CEC[=;==c2ctJ]d=¿Z6E/\\;bsbsbj]SSd=66jj]bddsbJ]j]d]d8"
+
+UNICODE_BRAILLE_MAP= [ \
+    0b00000001,
+    0b00001000,
+    0b00000010,
+    0b00010000,
+    0b00000100,
+    0b00100000,
+    0b01000000,
+    0b10000000,
+]
 
 class TermGraphics(object):
     def __init__(self, mode = MODE_BRAILLE):
@@ -18,23 +38,18 @@ class TermGraphics(object):
         self.term_shape = list(reversed(list(map(lambda x: int(x), os.popen('stty size', 'r').read().split()))))
         self.shape = (self.term_shape[0]*2, self.term_shape[1]*4)
         self.buffer = bytearray(self.term_shape[0]*self.term_shape[1])
+        self.colors = bytearray(self.term_shape[0]*self.term_shape[1])
+        self.current_color = 7
         self.mode = mode
-        self.unicode_braille_map = [ \
-          0b00000001,
-          0b00001000,
-          0b00000010,
-          0b00010000,
-          0b00000100,
-          0b00100000,
-          0b01000000,
-          0b10000000,
-        ]
 
     def clear(self):
         """
         Clear the graphics buffer.
         """
         self.buffer = bytearray(self.term_shape[0]*self.term_shape[1])
+
+    def set_color(self, color):
+        self.current_color = color
 
     def points(self, points):
         """
@@ -51,7 +66,8 @@ class TermGraphics(object):
         if point[0] >= 0 and point[1] >= 0 and point[0] < self.shape[0] and point[1] < self.shape[1]:
             index = ((point[0] >> 1) + (point[1] >> 2) * self.term_shape[0])
             self.buffer[index] = self.buffer[index] | \
-              self.unicode_braille_map[(point[0] & 0b1) | ((point[1] & 0b11) << 1)]
+              UNICODE_BRAILLE_MAP[(point[0] & 0b1) | ((point[1] & 0b11) << 1)]
+            self.colors[index] = self.current_color
 
     def line(self, point0, point1):
         print("Not yet supported")
@@ -63,13 +79,21 @@ class TermGraphics(object):
         """
         sys.stdout.write("\033[H")
 
+        current_draw_color = -1
+
         for j in range(self.term_shape[1]):
             sys.stdout.write("\033[" + str(j+1) + ";1H")
+
             for i in range(self.term_shape[0]):
+                index = i + j*self.term_shape[0]
+                if self.colors[index] != current_draw_color:
+                    current_draw_color = self.colors[index]
+                    sys.stdout.write("\033[3" + str(current_draw_color) + "m")
                 if self.mode == MODE_BRAILLE:
-                    sys.stdout.write(chr(0x2800 + self.buffer[i + j*self.term_shape[0]]))
+                    sys.stdout.write(chr(0x2800 + self.buffer[index]))
                 elif self.mode == MODE_EASCII:
-                    sys.stdout.write(TABLE_EASCII[self.buffer[i + j*self.term_shape[0]]])
+                    sys.stdout.write(TABLE_EASCII[self.buffer[index]])
+        sys.stdout.write("\033[37m")
         sys.stdout.flush()
 
 
