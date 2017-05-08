1a460aa5a5589ceee040e8d7e7a4f1e6215a5bc6
Dheera Venkatraman <dheera@dheera.net>
2017-05-08 00:47:01 +0000

    rm

--- TermGraphics.py
+++ TermGraphics.py
@@ -7,7 +7,6 @@ import sys
 import time
 
 MODE_BRAILLE = 0
-MODE_ASCII = 1
 MODE_EASCII = 2
 
 TABLE_EASCII = " '-'.*.|~~/F([-\\-~/>-&'\"\"~))//.\\\\\\_LLL'\"<C-=CC|)-\\vD=D|Y|Y|)AH.!i!.ii|/\"/F/Fff//rkfPrkJJ/P/P/P/?>brr>kl>&&*=fF/)vb/PPDJ)19/2/R.\\\\\\\\\\\\(=T([(((C=3-5cSct!919|7Ce,\\\\\\_\\\\\\i919i9(C|)\\\\+tv\\|719|7@9???L_LLL_=6[CEC[=;==c2ctJ]d=¿Z6E/_;bsbsbj]SSd=66jj]bddsDJ]j]d]d8"
