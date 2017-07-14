deef0a5c799a7f62d46e78057c63237269001b86
Dheera Venkatraman <dheera@dheera.net>
2017-07-14 06:45:49 +0000

    unicode buffer

--- termgraphics.py
+++ termgraphics.py
@@ -11,14 +11,18 @@ import time
 if sys.version_info >= (3,):
     unichr = chr
 
-COLOR_BLACK = 0
-COLOR_RED = 1
-COLOR_GREEN = 2
-COLOR_YELLOW = 3
-COLOR_BLUE = 4
-COLOR_MAGENTA = 5
-COLOR_CYAN = 6
-COLOR_WHITE = 7
+COLOR_BLACK = (0, 0, 0)
+COLOR_RED = (255, 0, 0)
+COLOR_GREEN = (0, 255, 0)
+COLOR_YELLOW = (255, 255, 0)
+COLOR_BLUE = (0, 0, 255)
+COLOR_MAGENTA = (255, 0, 255)
+COLOR_CYAN = (0, 255, 255)
+COLOR_WHITE = (255, 255, 255)
+
+COLOR_SUPPORT_1 = 0
+COLOR_SUPPORT_16 = 1
+COLOR_SUPPORT_256 = 2
 
 MODE_BRAILLE = 0
 MODE_EASCII = 2
@@ -46,32 +50,41 @@ class TermGraphics(object):
         Initialization. This class takes no arguments.
         """
         self.term_shape = list(reversed(list(map(lambda x: int(x), os.popen('stty size', 'r').read().split()))))
+        self.term_type = os.environ['TERM']
         self.shape = (self.term_shape[0]*2, self.term_shape[1]*4)
-        self.buffer = bytearray(self.term_shape[0]*self.term_shape[1])
-        self.colors = bytearray(self.term_shape[0]*self.term_shape[1])
+        self.buffer = bytearray(b'\x28\x00' * self.term_shape[0]*self.term_shape[1])
+        self.colors = bytearray(3*self.term_shape[0]*self.term_shape[1])
         self.buffer_text = bytearray(self.term_shape[0]*self.term_shape[1])
-        self.current_color = 7
+        self.current_color = (255, 255, 255)
         self.mode = mode
+
+        if self.term_type in ['xterm-256color', 'xterm']:
+          self.color_support = COLOR_SUPPORT_256
+        else:
+          self.color_support = COLOR_SUPPORT_16
+
+    def _rgb_to_8(rgb):
+        return (rgb[2] > 127) << 2 | (rgb[1] > 127)<<1 | (rgb[0] > 127)
 
     def clear(self):
         """
         Clear the graphics buffer.
         """
-        self.buffer = bytearray(self.term_shape[0]*self.term_shape[1])
-        self.colors = bytearray(self.term_shape[0]*self.term_shape[1])
+        self.buffer = bytearray(b'\x28\x00' * self.term_shape[0]*self.term_shape[1])
+        self.colors = bytearray(3*self.term_shape[0]*self.term_shape[1])
         self.buffer_text = bytearray(self.term_shape[0]*self.term_shape[1])
 
     def set_color(self, color):
         self.current_color = color
 
-    def points(self, points):
+    def points(self, points, clear_block = False):
         """
         Draws a list of points = [(x0,y0), (x1,y1), (x2,y2), ...].
         """
         for point in points:
-            self.point(point)
+            self.point(point, clear_block = clear_block)
 
-    def point(self, point):
+    def point(self, point, clear_block = False):
         """
         Draw a point at points = (x,y) where x is the column number, y is the row number, and (0,0)
         is the top-left corner of the screen.
@@ -81,9 +94,13 @@ class TermGraphics(object):
 
         if point[0] >= 0 and point[1] >= 0 and point[0] < self.shape[0] and point[1] < self.shape[1]:
             index = ((point[0] >> 1) + (point[1] >> 2) * self.term_shape[0])
-            self.buffer[index] = self.buffer[index] | \
-              UNICODE_BRAILLE_MAP[(point[0] & 0b1) | ((point[1] & 0b11) << 1)]
-            self.colors[index] = self.current_color
+            self.buffer[2*index] = 0x28
+            if clear_block:
+                self.buffer[2*index+1] = UNICODE_BRAILLE_MAP[(point[0] & 0b1) | ((point[1] & 0b11) << 1)]
+            else:
+                self.buffer[2*index+1] = self.buffer[2*index+1] | \
+                  UNICODE_BRAILLE_MAP[(point[0] & 0b1) | ((point[1] & 0b11) << 1)]
+            self.colors[3*index:3*index+3] = self.current_color
 
     def text(self, text, point):
         """
@@ -93,7 +110,7 @@ class TermGraphics(object):
         index = ((point[0] >> 1) + (point[1] >> 2) * self.term_shape[0])
         for i in range(len(text)):
             self.buffer_text[index + i] = ord(text[i])
-            self.colors[index + i] = self.current_color
+            self.colors[3*(index + i):3*(index+i)+3] = self.current_color
 
     def poly(self, points):
         """
@@ -169,16 +186,19 @@ class TermGraphics(object):
 
             for i in range(self.term_shape[0]):
                 index = i + j*self.term_shape[0]
-                if self.colors[index] != current_draw_color:
-                    current_draw_color = self.colors[index]
-                    sys.stdout.write("\033[3" + str(current_draw_color) + "m")
+                if self.colors[3*index:3*index+3] != current_draw_color:
+                    current_draw_color = self.colors[3*index:3*index+3]
+                    if self.color_support == COLOR_SUPPORT_256:
+                      sys.stdout.write("\033[38;2;{};{};{}m".format(current_draw_color[0], current_draw_color[1], current_draw_color[2]))
+                    else:
+                      sys.stdout.write("\033[3" + str(self._rgb_to_8(current_draw_color)) + "m")
                 if self.buffer_text[index]:
                     sys.stdout.write(chr(self.buffer_text[index]))
                 else:
                     if self.mode == MODE_BRAILLE:
-                        sys.stdout.write(unichr(0x2800 + self.buffer[index]))
+                        sys.stdout.write(unichr(self.buffer[2*index]<<8 | self.buffer[2*index+1]))
                     elif self.mode == MODE_EASCII:
-                        sys.stdout.write(TABLE_EASCII[self.buffer[index]])
+                        sys.stdout.write(TABLE_EASCII[self.buffer[2*index + 1]])
         sys.stdout.write("\033[37m")
         sys.stdout.flush()
 
